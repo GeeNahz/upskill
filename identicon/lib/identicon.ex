a6b177a5d7e4d73c2b558fae7df7213712659b33
Godswill Nasara <godswillnlm@gmail.com>
2025-12-22 10:13:29 +0100

    feat: add color picker function to extract
    This function takes in the Image struct with the hex value and uses that
    to extract the color for the identicon from the first 3 values of the
    hex

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,11 @@ defmodule Identicon do
   def main(input) do
     input
     |> has_input
+    |> pick_color
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def has_input(input) do
