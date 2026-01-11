a7b9834739f335e32b23ee456353caf7946f7738
Godswill Nasara <godswillnlm@gmail.com>
2026-01-11 19:11:06 +0100

    feat: add test resource routes

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -21,12 +21,34 @@ defmodule HelloWeb.Router do
     get "/", PageController, :home
     get("/hello", HelloController, :index)
     get("/hello/:messenger", HelloController, :show)
+
+    resources("/users", UserController) do
+      resources("/posts", PostController)
+    end
+
+    resources("/posts", PostController, only: [:index, :show])
+    resources("/comments", CommentController, except: [:delete])
+    resources("/reviews", ReviewController)
+  end
+
+  scope("/admin", HelloWeb.Admin) do
+    pipe_through :browser
+
+    resources("/images", ImageController)
+    resources("/reviews", ReviewController)
+    resources("/users", UserController)
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HelloWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HelloWeb do
+    pipe_through :api
+
+    scope("/v1", V1) do
+      resources("/images", ImageController)
+      resources("/reviews", ReviewController)
+      resources("/users", UserController)
+    end
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:hello, :dev_routes) do
