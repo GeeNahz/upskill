51e8e6583a8e925f04d38f6bd3dd2e6bb7b72fb8
Godswill Nasara <godswillnlm@gmail.com>
2026-01-13 22:14:33 +0100

    feat: impl json render, redirect, and flash messages

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -6,6 +6,14 @@ defmodule HelloWeb.HelloController do
   end
 
   def show(conn, %{"messenger" => messenger}) do
-    render(conn, :show, messenger: messenger)
+    # can use `assign/3` to pass in params and pipe down to render
+    conn
+    |> assign(:messenger, messenger)
+    |> assign(:receiver, "Dweezil")
+    |> render(:show)
+
+    # render(conn, :show, messenger: messenger, receiver: "Dweezil") # for html/template based response
+    # json(conn, %{id: messenger}) # for json-base response
+    # text(conn, "From messenger: #{messenger}") # for text-based response
   end
 end
